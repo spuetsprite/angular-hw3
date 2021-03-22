c221caa771d230cc36660defde5b5c7f66810e90
spuetsprite <jittakan.tum@student.mahidol.edu>
2021-03-22 17:24:18 +0700

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component'
+import { HeaderMenuComponent } from './header-menu/header-menu.component'
+import { FooterComponent } from './footer/footer.component'
+import { TopComponent } from './top/top.component'
+import { SearchComponent } from './search/search.component'
+import { MainComponent } from './main/main.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderMenuComponent,
+    FooterComponent,
+    TopComponent,
+    SearchComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule
